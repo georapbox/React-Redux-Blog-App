3b4a9348cc8b562d5f2268f9f905b0eaaf8d3ca7
George Raptis <georapbox@gmail.com>
2017-04-28 18:57:50 +0300

    Integrate redux-dev-tools

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {BrowserRouter} from 'react-router-dom';
 import reduxThunk from 'redux-thunk';
 import App from './components/app';
+import rootReducer from './reducers';
+import * as pkg from '../package.json';
 
-import reducers from './reducers';
+const composeEnhancers = process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    name: pkg.name,
+    actionsBlacklist: []
+  })
+  : compose;
 
-const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const enhancer = composeEnhancers(applyMiddleware(reduxThunk));
+const store = createStore(rootReducer, enhancer);
+
+// const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+// const store = createStoreWithMiddleware(rootReducer);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
